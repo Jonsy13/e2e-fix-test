ec78637d83ed0de145e6546adbb1e3fa75a39f18
Vedant Shrotria <vedant.shrotria@harness.io>
2022-05-15 00:04:08 +0530

    Added tests for tolerations
    Signed-off-by: Vedant Shrotria <vedant.shrotria@harness.io>

--- litmus/litmusctl_tests.sh
+++ litmus/litmusctl_tests.sh
@@ -10,8 +10,8 @@ agentName=${AGENT_NAME}
 projectName=${PROJECT_NAME}
 
 components="subscriber,chaos-exporter,chaos-operator-ce,event-tracker,workflow-controller"
-tolerations='[{"tolerationSeconds":0,"key":"special","value":"true","Operator":"Equal","effect":"NoSchedule"}]'
-nodeSelectors='beta.kubernetes.io/arch=amd64'
+defaultTolerations='[{"tolerationSeconds":0,"key":"special","value":"true","Operator":"Equal","effect":"NoSchedule"}]'
+defaultNodeSelectors='beta.kubernetes.io/arch=amd64'
 
 function configure_account(){
     litmusctl config set-account --endpoint="${accessPoint}" --username="admin" --password="litmus"
@@ -23,41 +23,50 @@ function agent_cleanup(){
     kubectl delete -f https://raw.githubusercontent.com/litmuschaos/litmus/master/litmus-portal/litmus-portal-crds.yml
 }
 
-# function test_install_with_nodeSelectors() {
-#     configure_account
+function configure_agent(){
+
+    nodeSelectors=$1
+
+    tolerations=$2
+
+    projectID=$(litmusctl get projects | grep "${projectName}" |  awk '{print $1}')
+
+    if [[ "$installation_mode" == "NS-MODE" ]];then
+
+        kubectl create ns ${namespace}
+        # Installing CRD's, required for namespaced mode
+        kubectl apply -f https://raw.githubusercontent.com/litmuschaos/litmus/master/litmus-portal/litmus-portal-crds.yml
+        
+        litmusctl create agent --agent-name=${agentName} --project-id=${projectID} --installation-mode=namespace --namespace=${namespace} --node-selector=${nodeSelectors} --tolerations=${tolerations} --non-interactive
+
+    else
+            
+        litmusctl create agent --agent-name=${agentName} --project-id=${projectID} --installation-mode=cluster --namespace=${namespace} --node-selector=${nodeSelectors} --tolerations=${tolerations} --non-interactive
     
-#     projectID=$(litmusctl get projects | grep "${projectName}" |  awk '{print $1}')
+    fi
 
-#     kubectl create ns ${namespace}
-#     # Installing CRD's, required for namespaced mode
-#     kubectl apply -f https://raw.githubusercontent.com/litmuschaos/litmus/master/litmus-portal/litmus-portal-crds.yml
+    wait_for_agent_to_be_ready      
+}
+
+function test_install_with_nodeSelectors() {
+    configure_account
+
+    configure_agent $defaultNodeSelectors ""
     
-#     litmusctl create agent --agent-name=${agentName}-1 --project-id=${projectID} --installation-mode=namespace --namespace=${namespace} --node-selector=$nodeSelectors --non-interactive
+    echo "Verifying nodeSelectors in all required Deployments"
 
-#     wait_for_agent_to_be_ready
+    for i in $(echo $components | sed "s/,/ /g")
+    do
+        verify_deployment_nodeselector ${i} ${namespace} '{"beta.kubernetes.io/arch":"amd64"}'
+    done
 
-#     echo "Verifying nodeSelectors in all required Deployments"
-
-#     for i in $(echo $components | sed "s/,/ /g")
-#     do
-#         verify_deployment_nodeselector ${i} ${namespace} '{"beta.kubernetes.io/arch":"amd64"}'
-#     done
-
-#     agent_cleanup
-# }
+    agent_cleanup
+}
 
 function test_install_with_tolerations() {
     configure_account
 
-    projectID=$(litmusctl get projects | grep "${projectName}" |  awk '{print $1}')
-
-    kubectl create ns ${namespace}
-    # Installing CRD's, required for namespaced mode
-    kubectl apply -f https://raw.githubusercontent.com/litmuschaos/litmus/master/litmus-portal/litmus-portal-crds.yml
-    
-    litmusctl create agent --agent-name=${agentName}-2 --project-id=${projectID} --installation-mode=namespace --namespace=${namespace} --tolerations=${tolerations} --non-interactive
-    
-    wait_for_agent_to_be_ready
+    configure_agent "" $defaultTolerations
 
     echo "Verifying tolerations in all required Deployments"
 
@@ -65,6 +74,14 @@ function test_install_with_tolerations() {
     do
         verify_deployment_tolerations ${i} ${namespace} '[{"effect":"NoSchedule","key":"special","operator":"Equal","value":"true"}]' 
     done
+
+    agent_cleanup
+}
+
+function test_native() {
+    configure_account
+
+    configure_agent "" ""
 
     agent_cleanup
 }
@@ -82,6 +99,17 @@ function wait_for_agent_to_be_ready(){
     verify_all_components $components ${namespace}
 }
 
-# test_install_with_nodeSelectors
-
-test_install_with_tolerations
+case ${1} in
+  test_with_nodeSelectors)
+    test_install_with_nodeSelectors
+    ;;
+  test_with_tolerations)
+    test_install_with_tolerations
+    ;;
+  test_native)
+    test_native
+    ;;
+  *)
+    echo "Invalid Arguments"
+    exit 1
+esac
