729140df703132536f98e27a530decafd238d2ca
Vedant Shrotria <vedant.shrotria@harness.io>
2022-06-26 21:25:02 +0530

    Added setup
    Signed-off-by: Vedant Shrotria <vedant.shrotria@harness.io>

--- litmus/install-portal.sh
+++ litmus/install-portal.sh
@@ -12,7 +12,7 @@ installation_mode=${INSTALLATION_MODE}
 function install_portal_cs_mode() {
 
     echo -e "\n---------------Installing Litmus-Portal in Cluster Scope----------\n"
-    curl https://raw.githubusercontent.com/litmuschaos/litmus/master/litmus-portal/cluster-k8s-manifest.yml --output litmus-portal-setup.yml
+    curl https://raw.githubusercontent.com/litmuschaos/litmus/master/litmus-portal/manifests/cluster-k8s-manifest.yml --output litmus-portal-setup.yml
     # manifest_image_update $version litmus/cluster-k8s-manifest.yml
 
     kubectl apply -f litmus-portal-setup.yml
@@ -23,9 +23,9 @@ function install_portal_ns_mode(){
     echo -e "\n---------------Installing Litmus-Portal in Namespace Scope----------\n"
     kubectl create ns ${namespace}
     # Installing CRD's, required for namespaced mode
-    kubectl apply -f https://raw.githubusercontent.com/litmuschaos/litmus/master/litmus-portal/litmus-portal-crds.yml
+    kubectl apply -f https://raw.githubusercontent.com/litmuschaos/litmus/master/litmus-portal/manifests/litmus-portal-crds.yml
 
-    kubectl apply -f https://raw.githubusercontent.com/litmuschaos/litmus/master/litmus-portal/namespace-k8s-manifest.yml -n ${namespace}
+    kubectl apply -f https://raw.githubusercontent.com/litmuschaos/litmus/master/litmus-portal/manifests/namespace-k8s-manifest.yml -n ${namespace}
 }
 
 
