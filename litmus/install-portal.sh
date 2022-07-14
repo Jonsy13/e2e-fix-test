d94847017f3169ca681019cd8862956ea3d7ad60
Vedant Shrotria <vedant.shrotria@harness.io>
2022-07-14 19:44:04 +0530

    Added docs for 2.11.0 release
    Signed-off-by: Vedant Shrotria <vedant.shrotria@harness.io>

--- litmus/install-portal.sh
+++ litmus/install-portal.sh
@@ -13,11 +13,11 @@ deploy_self_agent=${DEPLOY_SELF_AGENT:="true"}
 function install_portal_cs_mode() {
 
     echo -e "\n---------------Installing Litmus-Portal in Cluster Scope----------\n"
-    # curl https://raw.githubusercontent.com/litmuschaos/litmus/master/litmus-portal/manifests/cluster-k8s-manifest.yml --output litmus-portal-setup.yml
+    curl https://raw.githubusercontent.com/litmuschaos/litmus/master/litmus-portal/manifests/cluster-k8s-manifest.yml --output litmus-portal-setup.yml
     # manifest_image_update $version litmus/cluster-k8s-manifest.yml
 
-    # kubectl apply -f litmus-portal-setup.yml
-    kubectl apply -f litmus/cluster-k8s-manifest.yml
+    kubectl apply -f litmus-portal-setup.yml
+    # kubectl apply -f litmus/cluster-k8s-manifest.yml
 }
 
 function install_portal_ns_mode(){
@@ -27,8 +27,8 @@ function install_portal_ns_mode(){
     # Installing CRD's, required for namespaced mode
     kubectl apply -f https://raw.githubusercontent.com/litmuschaos/litmus/master/litmus-portal/manifests/litmus-portal-crds.yml
 
-    # kubectl apply -f https://raw.githubusercontent.com/litmuschaos/litmus/master/litmus-portal/manifests/namespace-k8s-manifest.yml -n ${namespace}
-    kubectl apply -f litmus/namespaced-k8s-template.yml -n ${namespace}
+    kubectl apply -f https://raw.githubusercontent.com/litmuschaos/litmus/master/litmus-portal/manifests/namespace-k8s-manifest.yml -n ${namespace}
+    # kubectl apply -f litmus/namespaced-k8s-template.yml -n ${namespace}
 }
 
 
